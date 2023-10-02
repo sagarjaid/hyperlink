10f92d56f9a301b1f74dbd9bfd2df895fd88fce1
sagarjaid <sagarjaid321@gmail.com>
2023-10-03 01:25:23 +0530

    1st fix

--- components/Footer.js
+++ components/Footer.js
@@ -2,20 +2,18 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='m-auto flex flex-col justify-center items-start gap-10 pt-5 max-w-6xl p-4 pb-10'>
-      <div className='m-auto flex justify-center gap-2 text-xs'>
-        <a
-          href='https://hyperlinkschool.com/privacy'
-          target='_blank'>
-          Privacy
-        </a>
-        <span>|</span>
-        <a
-          href='https://hyperlinkschool.com/tc'
-          target='_blank'>
-          T&C
-        </a>
-      </div>
+    <div className='flex justify-center gap-2 text-xs'>
+      <a
+        href='https://hyperlinkschool.com/privacy'
+        target='_blank'>
+        Privacy
+      </a>
+      <span>|</span>
+      <a
+        href='https://hyperlinkschool.com/tc'
+        target='_blank'>
+        T&C
+      </a>
     </div>
   );
 };
